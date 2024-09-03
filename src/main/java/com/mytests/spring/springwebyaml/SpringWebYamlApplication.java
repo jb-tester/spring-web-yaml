04e58c7cb2d96436c2193616682f7da879874753
irina.petrovskaya <irina.petrovskaya@jetbrains.com>
2024-09-03 09:23:28 +0200

    Initial commit

---
+++ src/main/java/com/mytests/spring/springwebyaml/SpringWebYamlApplication.java
@@ -0,0 +1,13 @@
+package com.mytests.spring.springwebyaml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebYamlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebYamlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mytests/spring/springwebyaml/SpringWebYamlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mytests.spring.springwebyaml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebYamlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
